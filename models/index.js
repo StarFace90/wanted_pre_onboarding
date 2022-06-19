15bbb05198d31495e9fe335239f6919782605b56
Woody-Park <starface1009@gmail.com>
2022-06-19 19:37:33 +0900

    [comment]: 주석 처리된 코드 제거

--- models/index.js
+++ models/index.js
@@ -21,20 +21,6 @@ sequelize
 db.sequelize.sync({ force: false }).then(() => {
   console.log("DB 삭제 및 재 연결");
 });
-//const env = process.env.NODE_ENV || "development";
-//const config = require(__dirname + "/../config/config.json")[env];
-
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
-// }
 
 const db = {};
 db.sequelize = Sequelize;
